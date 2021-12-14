096dc976f6998063f1110d80fc077492e65726d0
Diego Agudelo <diego.e.agudelo@gmail.com>
2021-12-13 20:20:49 -0500

    .

--- src/componentes/avances/graphql/mutations.js
+++ src/componentes/avances/graphql/mutations.js
@@ -2,18 +2,28 @@
 import { gql } from "@apollo/client";
 
 export const CREAR_AVANCE_POR_ID = gql`
-mutation crearAvancePorId_Proyecto( $descripcion: String!, $observacion: String!){
-    crearAvance( 
-      descripcion:$descripcion,
-      observacion:$observacion
-     ) { 
-      descripcion
-      observacion:[{observacion:observacion
-        fecha_observacion:""} ] 
+  mutation crearAvancePorId_Proyecto(
+    $id_proyecto: ID!
+    $id_usuario: ID!
+    $descripcion: String!
+    $fecha_avances: Date
+    $observacion: [ObservacionInput]
+  ) {
+    crearAvancePorId_Proyecto(
+      input: {
+        id_proyecto: $id_proyecto
+        id_usuario: $id_usuario
+        descripcion: $descripcion
+        fecha_avances: $fecha_avances
+        observacion: $observacion
       }
+    ) {
       _id
       id_usuario
-      id_proyecto    
-    }    
+      id_proyecto {
+        _id
+        nombre_proyecto
+      }
+    }
   }
 `;
